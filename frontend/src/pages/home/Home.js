1f1faeff32a49dc2a42f3b91feb22f48634bde88
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-03 12:48:58 +0200

    Page Heading Component

--- frontend/src/pages/home/Home.js
+++ frontend/src/pages/home/Home.js
@@ -3,6 +3,20 @@ import Slider from '../../components/slider/Slider'
 import "./Home.scss"
 import HomeInfoBox from './HomeInfoBox'
 
+const PageHeading = ({heading, btnText}) => {
+  return (
+    <>
+     <div className='--flex-between'>
+        <h2 className='--fw-thin'>{heading}</h2>
+        <button className='--btn'>
+           {btnText}
+        </button>
+     </div>
+     <div className='--hr'></div>
+    </>
+  )
+}
+
 const Home = () => {
   return (
     <>
@@ -10,6 +24,7 @@ const Home = () => {
       <section>
           <div className='container'>
             <HomeInfoBox />
+            <PageHeading heading={"Latest Products"} btnText={"Shop Now>>>"}/>
           </div>
       </section> 
     </>
