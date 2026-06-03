afcd2d9503fb36eed9e92706ce7f7ffd1a33591d
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-03 04:04:20 +0200

    Header Component Logo and Nav Menu

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/home/Home'
+import Header from './components/header/Header'
 
 
 const App = () => {
   return (
     <>
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
