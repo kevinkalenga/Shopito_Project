05a1db5f34d9e17d4a7fc1573a6f52a2654f6e29
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-03 11:02:42 +0200

    Slider Component Logic

--- frontend/src/components/slider/Slider.js
+++ frontend/src/components/slider/Slider.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import "./Slider.scss"
 import {AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai"
 import { sliderData } from './slider-data'
 import {useNavigate} from 'react-router-dom'
@@ -7,14 +8,38 @@ const Slider = () => {
     // imgbb.com et compte crée avec google
   
     const [currentSlide, setCurrentSlide] = useState(0) 
-
+    const slideLength = sliderData.length 
+    const autoScroll = true
+    
+    let slideInterval;
+    const intervalTime = 5000
+    
+    
+    
     const navigate = useNavigate()
     
     
-    const prevSlide = () => {}
-    const nextSlide = () => {}
+    const prevSlide = () => {
+       setCurrentSlide(currentSlide === 0 ? slideLength - 1 : currentSlide - 1)
+    }
+    const nextSlide = () => {
+      setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1)
+    }
   
-  
+   useEffect(() => {
+    setCurrentSlide(0)
+   }, [])
+   
+   useEffect(() => {
+    if(autoScroll) {
+      const auto  = () => {
+        slideInterval = setInterval(nextSlide, intervalTime)
+      }
+      auto()
+    }
+
+    return () => clearInterval(slideInterval)
+   }, [currentSlide, intervalTime, autoScroll])
   
     return (
     <div className='slider'>
