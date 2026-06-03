fdcddcb41d17067f805ada03264be258609c7207
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-03 10:10:46 +0200

    Footer Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/home/Home'
 import Header from './components/header/Header'
+import Footer from './components/footer/Footer'
 
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
+            <Footer />
         </BrowserRouter>
     </>
   )
