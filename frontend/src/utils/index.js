69fa82b1cce87cf0c6c76f770c80d352accfca43
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-04 09:21:49 +0200

    Carousel Item Component

---
+++ frontend/src/utils/index.js
@@ -0,0 +1,10 @@
+// function created to shorten the text
+export const shortenText = (text, n) => {
+    if(text.length > n) {
+        const shortenedText = text.substring(0, n).concat("..."); 
+
+        return shortenedText;
+    }
+
+    return text;
+};
