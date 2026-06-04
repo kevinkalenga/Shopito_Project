b0c173cd177eda328116098670f6982eab972091
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-04 12:50:31 +0200

    Make the Header Sticky

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -20,6 +20,17 @@ const activeLink = ({isActive}) => (isActive ? `${styles.active}` : "")
 const Header = () => {
   
    const [showMenu, setShowMenu] = useState(false)
+   const [scrollPage, setScrollPage] = useState(false)
+
+   const fixNavbar = () => {
+      if(window.scrollY > 50) {
+         setScrollPage(true)
+      } else {
+         setScrollPage(false)
+      }
+   };
+
+   window.addEventListener("scroll", fixNavbar)
    
    const toggleMenu = () => {
       setShowMenu(!showMenu)
@@ -39,7 +50,7 @@ const Header = () => {
    )
   
   return (
-      <header>
+      <header className={scrollPage ? `${styles.fixed}` : null}>
         <div className={styles.header}>
            {logo}
            <nav className={showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`}>
