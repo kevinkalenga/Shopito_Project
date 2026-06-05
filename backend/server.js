97f30bfd83f60e2c57fe0a979b42616d54935d60
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-05 10:43:07 +0200

    Create Server and Connect MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,21 @@
-console.log("Hello World!!!");
+const dotenv = require("dotenv").config();
+const express = require('express');
+const mongoose = require("mongoose");
+const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+const app = express() 
+
+// Routes 
+app.get("/", (req, res) => {
+    res.send("Home Page ....")
+})
+
+const PORT  = process.env.PORT || 5000
+
+mongoose.connect(process.env.MONGO_URI).then(() => {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`)
+        console.log("mongodb connected")
+    })
+}).catch((err) => console.log(err))
