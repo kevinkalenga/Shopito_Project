5a33baa9516f0d3949ca9c6538eb2729c97e2291
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-12 00:38:47 +0200

    Loader Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,17 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/home/Home'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
+import { Spinner } from './components/loader/Loader'
+// import Loader from './components/loader/Loader'
 
 
 const App = () => {
   return (
     <>
         <BrowserRouter>
+            
             <Header />
+            <Spinner />
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
