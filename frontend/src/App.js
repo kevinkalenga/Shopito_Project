6a917aa983a18138e265013e626723891155366f
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-12 00:46:12 +0200

    Card Component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Home from './pages/home/Home'
 import Header from './components/header/Header'
 import Footer from './components/footer/Footer'
-import { Spinner } from './components/loader/Loader'
-// import Loader from './components/loader/Loader'
+
 
 
 const App = () => {
@@ -13,7 +12,7 @@ const App = () => {
         <BrowserRouter>
             
             <Header />
-            <Spinner />
+           
             <Routes>
                 <Route path="/" element={<Home />} />
             </Routes>
