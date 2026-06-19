d8a298b360c6472d1c72ff7496eafec3c8eeedda
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-19 12:20:08 +0200

    Profile Page - JSX

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -1,14 +1,86 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Profile.scss"
 import PageMenu from '../../components/pageMenu/PageMenu'
+import Card from '../../components/card/Card';
+import {useSelector} from "react-redux"
+
+
 
 const Profile = () => {
+    const { isLoading, user } = useSelector(
+      (state) => state.auth
+    );
+
+    const initialState = {
+      name: user?.name || "",
+      email: user?.email || "",
+      phone: user?.phone || "",
+      role: user?.role || "",
+      address: user?.address || {}
+
+    };
+   
+    const [profile, setProfile] = useState(initialState);
+
+    const saveProfile = async() => {
+
+    }
+    const handleImageChange = () => {}
+    const handleInputChange = () => {}
+  
+  
   return (
     <>
       <section>
         <div className='container'>
           <PageMenu />
           <h2>Profile</h2>
+          <div className='--flex-start profile'>
+              <Card cardClass={"card"}>
+                 {
+                  !isLoading && (
+                     <>
+                       <div className='profile-photo'>
+                          <h2>Profile Image</h2>
+                       </div>
+                       <form onSubmit={saveProfile}>
+                          <p>
+                            <label>Change Photo:</label>
+                            <input type="file" accept='image/*' name="image" onChange={handleImageChange} />
+                          </p>
+                          <p>
+                            <label>Name:</label>
+                            <input type="text" name='name' value={profile?.name} onChange={handleInputChange} />
+                          </p>
+                          <p>
+                            <label>Email:</label>
+                            <input type="email" name='email' value={profile?.email} onChange={handleInputChange} disabled />
+                          </p>
+                          <p>
+                            <label>Phone:</label>
+                            <input type="text" name='phone' value={profile?.phone} onChange={handleInputChange} />
+                          </p>
+                          <p>
+                            <label>Address:</label>
+                            <input type="text" name='address' value={profile?.address?.address} onChange={handleInputChange} />
+                          </p>
+                          <p>
+                            <label>State:</label>
+                            <input type="text" name='state' value={profile?.address?.state} onChange={handleInputChange} />
+                          </p>
+                          <p>
+                            <label>Country:</label>
+                            <input type="text" name='country' value={profile?.address?.country} onChange={handleInputChange} />
+                          </p>
+                          <button className='--btn --btn-primary --btn-block'>
+                             Update Profile
+                          </button>
+                       </form>
+                     </>
+                  )
+                 }
+              </Card>
+          </div>
         </div>
       </section>
     </>
