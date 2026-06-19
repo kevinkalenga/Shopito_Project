992a4a0c6d459053d6cc03d0cc84f4c617b9f9c4
kevin kalenga <kevinkalenga10@gmail.com>
2026-06-19 13:47:53 +0200

    Profile Page - Update User

--- frontend/src/pages/profile/Profile.js
+++ frontend/src/pages/profile/Profile.js
@@ -3,7 +3,8 @@ import "./Profile.scss"
 import PageMenu from '../../components/pageMenu/PageMenu'
 import Card from '../../components/card/Card';
 import { useDispatch, useSelector } from "react-redux";
-import { getUser } from '../../redux/features/auth/authSlice';
+import { getUser, updateUser } from '../../redux/features/auth/authSlice';
+import Loader from '../../components/loader/Loader';
 
 
 
@@ -44,17 +45,56 @@ const Profile = () => {
     }, [user])
    
     const handleImageChange = () => {}
-    const handleInputChange = (e) => {
-      const {name, value} = e.target;
-      setProfile({...profile, [name]: value})
-    }
-    const saveProfile = async() => {
+    // const handleInputChange = (e) => {
+    //   const {name, value} = e.target;
+    //   setProfile({...profile, [name]: value})
+    // }
 
+       const handleInputChange = (e) => {
+          const { name, value } = e.target;
+
+            if (name === "address" || name === "state" || name === "country") {
+              setProfile({
+                ...profile,
+                address: {
+                  ...profile.address,
+                  [name]: value
+                }
+              });
+            } else {
+              setProfile({
+                ...profile,
+                [name]: value
+              });
+            }
+        };
+    
+    
+    
+    
+    const saveProfile = async(e) => {
+       e.preventDefault() 
+
+       const userData = {
+         name: profile.name,
+         phone: profile.phone,
+         address: {
+           address: profile.address.address,
+           state: profile.address.state,
+           country: profile.address.country,
+         }
+         
+       }
+      // console.log(userData)
+      await dispatch(updateUser(userData))
     }
   
   return (
     <>
       <section>
+        {
+          isLoading && <Loader />
+        }
         <div className='container'>
           <PageMenu />
           <h2>Profile</h2>
@@ -73,7 +113,7 @@ const Profile = () => {
                           </p>
                           <p>
                             <label>Name:</label>
-                            <input type="text" name='name' value={profile?.name} onChange={handleInputChange} />
+                            <input type="text" name='name' value={profile?.name} onChange={handleInputChange} required/>
                           </p>
                           <p>
                             <label>Email:</label>
@@ -81,19 +121,19 @@ const Profile = () => {
                           </p>
                           <p>
                             <label>Phone:</label>
-                            <input type="text" name='phone' value={profile?.phone} onChange={handleInputChange} />
+                            <input type="text" name='phone' value={profile?.phone} onChange={handleInputChange} required/>
                           </p>
                           <p>
                             <label>Address:</label>
-                            <input type="text" name='address' value={profile?.address?.address || ""} onChange={handleInputChange} />
+                            <input type="text" name='address' value={profile?.address?.address || ""} onChange={handleInputChange} required/>
                           </p>
                           <p>
                             <label>State:</label>
-                            <input type="text" name='state' value={profile?.address?.state} onChange={handleInputChange} />
+                            <input type="text" name='state' value={profile?.address?.state || ""} onChange={handleInputChange} required/>
                           </p>
                           <p>
                             <label>Country:</label>
-                            <input type="text" name='country' value={profile?.address?.country} onChange={handleInputChange} />
+                            <input type="text" name='country' value={profile?.address?.country || ""} onChange={handleInputChange} required/>
                           </p>
                           <button className='--btn --btn-primary --btn-block'>
                              Update Profile
