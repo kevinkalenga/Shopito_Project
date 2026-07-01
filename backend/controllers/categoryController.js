ff105d7a1005c2a8b3284097cceec5312c616301
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-01 14:17:38 +0200

    Create Category Controller

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -1,9 +1,26 @@
 const asyncHandler = require("express-async-handler");
-
+const Category = require("../models/categoryModel");
+const slugify = require("slugify")
 
 const createCategory = asyncHandler(async (req, res) => {
-   res.send("Create Category")
+   const {name} = req.body || {}
+   if(!name) {
+     res.status(400);
+        throw new Error("Please fill in category name")
+   }
 
+   const categoryExists = await Category.findOne({name});
+    if(categoryExists) {
+     res.status(400);
+        throw new Error("Category name already exists")
+    }
+
+    const category = await Category.create({
+       name,
+       slug: slugify(name)
+    })
+
+    res.status(201).json(category);
 });
 
 
