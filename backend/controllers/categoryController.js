2e713426e29479ad9ca05971f700da1dfbf1f683
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-01 14:48:31 +0200

    Delete Category - Route and Controller

--- backend/controllers/categoryController.js
+++ backend/controllers/categoryController.js
@@ -23,15 +23,30 @@ const createCategory = asyncHandler(async (req, res) => {
     res.status(201).json(category);
 });
 
- // get Products 
+
  const getCategories = asyncHandler(async (req, res) => {
     // sorting base on the creation date
    const categories = await Category.find().sort("-createdAt")
     res.status(200).json(categories)
 })
 
+ const deleteCategory = asyncHandler(async (req, res) => {
+   const slug = req.params.slug.toLowerCase()
+   const category = await Category.findOneAndDelete({slug});
+   if(!category) {
+       res.status(400)
+        throw new Error("Category not found")
+   }
+   
+
+   res.status(200).json({
+    message: "Category has been deleted"
+   });
+})
+
 
 module.exports = {
   createCategory,
-  getCategories
+  getCategories,
+  deleteCategory
 }
--- backend/routes/categoryRoute.js
+++ backend/routes/categoryRoute.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const {createCategory, getCategories} = require("../controllers/categoryController");
+const {createCategory, getCategories, deleteCategory} = require("../controllers/categoryController");
 const {protect, adminOnly} = require("../middleware/authMiddleware")
 
 
 router.post("/createCategory", protect, adminOnly, createCategory);
 router.get("/getCategories", protect, adminOnly, getCategories);
+router.delete("/:slug", protect, adminOnly, deleteCategory);
 
 
 
