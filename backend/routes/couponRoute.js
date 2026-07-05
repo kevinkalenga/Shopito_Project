33d7aac961c9c80f118865b00f1a74a57430724d
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-06 01:51:38 +0200

    Order - Model

--- backend/routes/couponRoute.js
+++ backend/routes/couponRoute.js
@@ -9,8 +9,8 @@ const {protect, adminOnly} = require("../middleware/authMiddleware")
 router.post("/createCoupon", protect, adminOnly, createCoupon);
 router.get("/getCoupons", protect, adminOnly, getCoupons);
 router.get("/:couponName", protect, getCoupon);
-router.put("/:id", updateCoupon);
-router.delete("/:id", deleteCoupon);
+router.put("/:id", protect, adminOnly, updateCoupon);
+router.delete("/:id", protect, adminOnly, deleteCoupon);
 
 
 
