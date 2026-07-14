908cceeda7d8bf6ff4704d517fab842a2b16bba8
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-14 20:27:42 +0200

    Create Brand - JSX

--- frontend/src/components/admin/brand/CreateBrand.js
+++ frontend/src/components/admin/brand/CreateBrand.js
@@ -1,8 +1,84 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Card from '../../card/Card'
+import {useDispatch, useSelector} from 'react-redux'
+import {toast} from 'react-toastify'
+import { createBrand } from '../../../redux/features/categoryAndbrand/categoryAndbrandSlice'
+import { getCategories} from '../../../redux/features/categoryAndbrand/categoryAndbrandSlice'
+import Loader from '../../loader/Loader'
 
 const CreateBrand = () => {
+  
+   const [name, setName] = useState("")
+   const [category, setCategory] = useState("")
+
+   const {isLoading} = useSelector((state) => state.brand)
+   const {categories} = useSelector((state) => state.category)
+   const dispatch = useDispatch()
+   
+
+    useEffect(() => {
+         dispatch(getCategories())
+    }, [dispatch])
+   
+   const saveBrand = async(e) => {
+      e.preventDefault() 
+
+      if(name.length < 3) {
+        return toast.error("Coupon must be up to 3 characters")
+      }
+
+      const formData = {
+         name
+      }
+
+      dispatch(createBrand(formData))
+      setName("")
+   }
+  
+  
+  
   return (
-    <div>CreateBrand</div>
+    <>
+      {isLoading && <Loader />}
+      <div className='--mb2'>
+         <h3>Create Brand</h3>
+         <p>
+            Use the form to <b>Create a Brand.</b>
+         </p>
+         <Card cardClass={"card"}>
+            <br /> 
+            <form onSubmit={saveBrand}>
+              <label>Brand Name:</label> 
+              <input 
+                type="text" 
+                placeholder='Brand name'
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <label>Parent Category:</label> 
+              
+              <select name='category' className='form-control' onChange={(e) => setCategory(e.target.value)}>
+                 <option>Select Category</option>
+                 {
+                  categories.length > 0 && categories.map((cat) => (
+                    <option key={cat._id} value={cat.name}>{cat.name}</option>
+                  ))
+                 }
+              </select>
+              
+              
+              <div className='--my'>
+                 <button type="submit" className='--btn --btn-primary'>
+                   Save Brand
+                 </button>
+              </div>
+            </form>
+         </Card>
+      </div>
+    
+    </>
   )
 }
 
--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,10 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit" 
 import authReducer from "../redux/features/auth/authSlice"
 import categoryReducer from "../redux/features/categoryAndbrand/categoryAndbrandSlice"
+import brandReducer from "../redux/features/categoryAndbrand/categoryAndbrandSlice"
 
 export const store = configureStore({
     reducer: {
        auth: authReducer,
        category: categoryReducer,
+       brand: brandReducer,
     }
 })
