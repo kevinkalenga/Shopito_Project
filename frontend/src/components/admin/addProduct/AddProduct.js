89057dfb30c1b16a994f8bc8445a36793233f5ef
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-21 01:34:13 +0200

    Create Product - JSX - Part1

--- frontend/src/components/admin/addProduct/AddProduct.js
+++ frontend/src/components/admin/addProduct/AddProduct.js
@@ -1,8 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSelector } from "react-redux";
+import Loader from '../../loader/Loader';
+import ProductForm from '../productForm/ProductForm';
+import "./AddProduct.scss";
+
+const initialState = {
+  name: "",
+  category: "",
+  brand: "",
+  quantity: "",
+  color: "",
+  price: "",
+  regularPrice: "",
+}
 
 const AddProduct = () => {
+  
+  const [product, setProduct] = useState(initialState);
+  const {name, category, brand, quantity, color, price, regularPrice} = product
+  const {isLoading} = useSelector((state) => state.product)
+  const {categories} = useSelector((state) => state.category);
+  const {brands} = useSelector((state) => state.brand);
+
+  const handleInputChange = (e) => {
+     const {name, value} = e.target; 
+     setProduct({...product, [name]: value})
+  }
+
+  const saveProduct = async () => {}
+  
   return (
-    <div>addProduct</div>
+    <section>
+       <div className='container'>
+          {isLoading && <Loader />}
+          <h3 className='--mt'>Add New Product</h3>
+
+          <ProductForm saveProduct={saveProduct} product={product} handleInputChange={handleInputChange} />
+       </div>
+    </section>
   )
 }
 
--- frontend/src/components/admin/productForm/productForm.js
+++ frontend/src/components/admin/productForm/productForm.js
@@ -1,8 +1,26 @@
-import React from 'react'
+import React from 'react';
+import "./ProductForm.scss"
+import Card from '../../card/Card';
 
-const productForm = () => {
+const productForm = ({saveProduct, product, handleInputChange}) => {
   return (
-    <div>productForm</div>
+    <div className='add-product'>
+       <h3>Upload Widget Placeholder</h3>
+
+       <Card cardClass={"card"}>
+          <br />
+          <form onSubmit={saveProduct}>
+             <label>Product Name:</label>
+             <input 
+               type="text"
+               placeholder='Product name'
+               name='name'
+               value={product.name}
+               onChange={handleInputChange}
+             />
+          </form>
+       </Card>
+    </div>
   )
 }
 
