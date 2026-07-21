bcfde5da75ba1b578f4d55f9dfbe1373d0ec98a4
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-21 02:09:03 +0200

    Create Product - JSX - Part2 - Categories input

--- frontend/src/components/admin/addProduct/AddProduct.js
+++ frontend/src/components/admin/addProduct/AddProduct.js
@@ -1,8 +1,9 @@
-import React, { useState } from 'react'
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import { useSelector, useDispatch } from "react-redux";
 import Loader from '../../loader/Loader';
 import ProductForm from '../productForm/ProductForm';
 import "./AddProduct.scss";
+import { getCategories } from '../../../redux/features/categoryAndbrand/categoryAndbrandSlice';
 
 const initialState = {
   name: "",
@@ -15,19 +16,26 @@ const initialState = {
 }
 
 const AddProduct = () => {
-  
+  const dispatch = useDispatch();
   const [product, setProduct] = useState(initialState);
   const {name, category, brand, quantity, color, price, regularPrice} = product
   const {isLoading} = useSelector((state) => state.product)
   const {categories} = useSelector((state) => state.category);
   const {brands} = useSelector((state) => state.brand);
 
+  useEffect(() => {
+    dispatch(getCategories())
+  }, [dispatch])
+
   const handleInputChange = (e) => {
      const {name, value} = e.target; 
      setProduct({...product, [name]: value})
   }
 
-  const saveProduct = async () => {}
+  const saveProduct = async (e) => {
+     e.preventDefault()
+     console.log(product)
+  }
   
   return (
     <section>
@@ -35,7 +43,10 @@ const AddProduct = () => {
           {isLoading && <Loader />}
           <h3 className='--mt'>Add New Product</h3>
 
-          <ProductForm saveProduct={saveProduct} product={product} handleInputChange={handleInputChange} />
+          <ProductForm 
+            saveProduct={saveProduct}
+            product={product} 
+            handleInputChange={handleInputChange} categories={categories} brands={brands} isEditing={false} />
        </div>
     </section>
   )
--- frontend/src/components/admin/productForm/productForm.js
+++ frontend/src/components/admin/productForm/productForm.js
@@ -2,7 +2,7 @@ import React from 'react';
 import "./ProductForm.scss"
 import Card from '../../card/Card';
 
-const productForm = ({saveProduct, product, handleInputChange}) => {
+const productForm = ({saveProduct, product, handleInputChange, categories, brands, isEditing,}) => {
   return (
     <div className='add-product'>
        <h3>Upload Widget Placeholder</h3>
@@ -15,9 +15,28 @@ const productForm = ({saveProduct, product, handleInputChange}) => {
                type="text"
                placeholder='Product name'
                name='name'
-               value={product.name}
+               value={product?.name}
                onChange={handleInputChange}
              />
+             <label>Product Category:</label>
+              <select name="category" value={product?.category} onChange={handleInputChange}>
+                 {isEditing ? (
+                   <option value={product?.category}>{product?.category}</option>
+                 ):(
+                  <option>Select Category</option>
+                 )}
+                 {categories.length > 0 && categories.map((cat) => (
+                   <option value={cat.name} key={cat._id}>{cat.name}</option>
+                 ))}
+              </select>
+              
+              
+              
+              <div className='--my'>
+                 <button type="submit" className='--btn --btn-primary'>
+                   Save Product
+                 </button>
+              </div>
           </form>
        </Card>
     </div>
