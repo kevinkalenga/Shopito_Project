7d452596c4ba0aae2a462a456dd6fddc68134717
kevin kalenga <kevinkalenga10@gmail.com>
2026-07-31 02:07:58 +0200

    Create Coupon - JSX

--- frontend/src/components/admin/coupon/CreateCoupon.js
+++ frontend/src/components/admin/coupon/CreateCoupon.js
@@ -1,8 +1,75 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
+import Card from '../../card/Card'
+import DatePicker from "react-datepicker";
+import Loader from '../../loader/Loader'
+
 
 const CreateCoupon = () => {
+   const [name, setName] = useState("")
+   const [discount, setDiscount] = useState(0)
+   const [expiresAt, setExpiresAt] = useState(new Date())
+
+   const {isLoading} = useSelector((state) => state.category)
+   const dispatch = useDispatch()
+   
+   
+   const saveCoupon = async(e) => {
+      e.preventDefault() 
+
+  
+
+     
+   }
+  
+  
+  
   return (
-    <div>CreateCoupon</div>
+    <>
+      {isLoading && <Loader />}
+      <div className='--mb2'>
+         <h3>Create Coupon</h3>
+         <p>
+            Use the form to <b>Create a Coupon.</b>
+         </p>
+         <Card cardClass={"card"}>
+            <br /> 
+            <form onSubmit={saveCoupon}>
+              <label>Coupon Name:</label> 
+              <input 
+                type="text" 
+                placeholder='Coupon name'
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                required
+              />
+              <label>Coupon Discount:</label> 
+              <input 
+                type="number" 
+                placeholder='Coupon discount'
+                name='discount'
+                value={discount}
+                onChange={(e) => setDiscount(e.target.value)}
+                required
+              />
+              <label>Expiry Date :</label> 
+              <DatePicker 
+                selected={expiresAt}
+                value={expiresAt}
+                onChange={(date) => setExpiresAt(date)}
+                required
+              />
+              <div className='--my'>
+                 <button type="submit" className='--btn --btn-primary'>
+                   Save Coupon
+                 </button>
+              </div>
+            </form>
+         </Card>
+      </div>
+    
+    </>
   )
 }
 
