6b3fab39d00ca2d477b48c3436053e903214e6c0
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-02 12:30:47 +0200

    Shop Page Layout - Part2

--- frontend/src/pages/shop/Product.js
+++ frontend/src/pages/shop/Product.js
@@ -4,6 +4,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getProducts } from '../../redux/features/product/productSlice';
 import ProductFilter from '../../components/product/productFilter/ProductFilter';
 import ProductList from '../../components/product/productList/ProductList';
+import { Spinner } from '../../components/loader/Loader';
+import { FaCogs } from "react-icons/fa";
 
 const Product = () => {
   const dispatch = useDispatch()
@@ -25,11 +27,21 @@ const Product = () => {
   return (
     <section>
         <div className={`container ${styles.product}`}>
-          <aside>
-            <ProductFilter />
+          <aside className={showFilter ? `${styles.filter} ${styles.show}` : `${styles.filter}`}>
+              {
+                isLoading ? null : <ProductFilter />
+              }  
           </aside>
           <div className={styles.content}>
-            <ProductList />
+              {
+                isLoading ? <Spinner /> :  <ProductList products={products} />
+              }  
+              <div className={styles.icon} onClick={toggleFilter}>
+                 <FaCogs size={20} color="orangered" />
+                 <p>
+                   <b>{showFilter ? "Hide Filter" : "Show Filter"}</b>
+                 </p>
+              </div>
           </div>
         </div>
     </section>
