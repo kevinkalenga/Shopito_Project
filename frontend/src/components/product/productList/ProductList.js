1019ba1b88d559812b27d390a79ae758bea60a44
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-02 13:31:10 +0200

    ProductList Component - Part1

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import styles from "./ProductList.module.scss"
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import Search from '../../search/Search';
 
-const ProductList = () => {
+const ProductList = ({products}) => {
   return (
-    <div>
-        <h3>ProductList</h3>
+    <div className={styles["product-list"]}>
+       <div className={styles.top}>
+         <div className={styles.icons}>
+            <BsFillGridFill 
+             size={22} 
+             color="orangered"
+            //  onClick={() => setGrid(true)}
+            />
+            <FaListAlt 
+             size={24} 
+             color="#0066d4"
+            //  onClick={() => setGrid(false)}
+            />
+            <p>
+              <strong>{products.length} Products found</strong>
+            </p>
+         </div>
+         <div>
+           <Search />
+         </div>
+         <div className={styles.sort}>
+            <label>Sort by:</label>
+            <section>
+              <option value="latest">Latest</option>
+              <option value="lowest-price">Lowest Price</option>
+              <option value="highest-price">highest Price</option>
+              <option value="a-z">A - Z</option>
+              <option value="z-a">Z - A</option>
+             
+            </section>
+         </div>
+       </div>
     </div>
   )
 }
