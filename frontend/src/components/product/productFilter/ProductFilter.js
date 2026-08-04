395dc52ebf66e4d0b72785a76e41c11702a92642
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-04 18:25:52 +0200

    Filter Product By Category - Part1

--- frontend/src/components/product/productFilter/ProductFilter.js
+++ frontend/src/components/product/productFilter/ProductFilter.js
@@ -1,9 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {useSelector} from "react-redux"
+import styles from "./ProductFilter.module.scss"
 
 const ProductFilter = () => {
+  
+  const {products, minPrice, maxPrice} = useSelector((state) => state.product)
+
+  const [category, setCategory] = useState("All")
+  
+  const allCategories = [
+    "All",
+    ...new Set(products?.map((product) => product.category))
+  ]
+  //console.log(allCategories)
+
+  const filterProductCategory = (cat) => {
+     console.log(cat)
+  }
+  
   return (
-    <div>
-        <h3>ProductFilter</h3>
+    <div className={styles.filter}>
+        <h4>Categories</h4>
+          <div className={styles.category}>
+            {
+              allCategories.map((cat, index) => {
+                return (
+                  <button 
+                    key={index}
+                    type='button'
+                    className={`${category}` === cat ? `${styles.active}` : null}
+                    onClick={() => filterProductCategory(cat)}
+                    >
+                    &#8250; {cat}
+                  </button>
+                )
+              })
+            }
+          </div>
     </div>
   )
 }
