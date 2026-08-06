50de86b17908f013c1216937e41191f435591d52
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-06 17:27:42 +0200

    Product Details - Image Slider Part2

--- frontend/src/components/loader/Loader.js
+++ frontend/src/components/loader/Loader.js
@@ -18,7 +18,7 @@ export const Spinner = () => {
         <div className='--center-all'>
              <img src={loaderImg} alt="loading" width={40} />
         </div>
-    )
+    );
 }
 
 export default Loader;
--- frontend/src/components/product/productDetails/ProductDetails.js
+++ frontend/src/components/product/productDetails/ProductDetails.js
@@ -1,11 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./ProductDetails.module.scss"
+import {useParams, Link} from "react-router-dom"
+import {useDispatch, useSelector} from "react-redux"
+import { getProduct } from '../../../redux/features/product/productSlice'
+import { Spinner } from '../../loader/Loader'
 
 const ProductDetails = () => {
+  
+  const {id} = useParams();
+  const dispatch = useDispatch();
+  const {product, isLoading} = useSelector((state) => state.product)
+  const [imageIndex, setImageIndex] = useState(0)
+
+  useEffect(() => {
+    dispatch(getProduct(id))
+  }, [dispatch, id])
+  // console.log(product)
+  
   return (
-    <div>
-        <h2>ProductDetails</h2>
-    </div>
+    <section>
+        <div className={`container ${styles.product}`}>
+           <h2>Product Details</h2>
+           <div>
+             <Link to="/shop">&larr; Back To Products</Link>
+           </div>
+           {
+            isLoading ? (<Spinner />) : (
+              <>
+                <div className={styles.details}>
+                   <div className={styles.img}>
+                      <img src={product?.image[imageIndex]} alt={product?.name} className={styles.pImg} />
+                      <div className={styles.smallImg}>
+                         {
+                          product?.image.map((img, index) => {
+                            return (
+                              <img key={index} src={img} alt='product image' 
+                              onClick={() => setImageIndex(index)} className={imageIndex === index ? "activeImg" : ""} />
+                            )
+                          })
+                         }
+                      </div>
+                   </div>
+                </div>
+              </>
+            )
+           }
+        </div>
+    </section>
   )
 }
 
