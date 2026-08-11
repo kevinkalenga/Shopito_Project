3e59151d669d90fe3289e4b75764fa89b2f90066
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-12 01:38:52 +0200

    Download Order Details as PDF

--- frontend/src/pages/order/OrderDetails.js
+++ frontend/src/pages/order/OrderDetails.js
@@ -1,13 +1,16 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { useNavigate, useParams, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { getOrder } from '../../redux/features/order/orderSlice';
 import { Spinner } from '../../components/loader/Loader';
+import html2canvas from "html2canvas"
+import jsPDF from "jspdf";
 
 const OrderDetails = () => {
    const navigate = useNavigate();
    const dispatch = useDispatch();
    const {id} = useParams()
+   const pdfRef = useRef()
    
    const {isLoading, order} = useSelector((state) => state.order)
 
@@ -15,14 +18,40 @@ const OrderDetails = () => {
        dispatch(getOrder(id))
      }, [dispatch, id])
 
-     const downloadPdf = () => {
+    const downloadPdf = () => {
       
-     }
+           const input = pdfRef.current;
+           html2canvas(input).then((canvas) => {
+             const imgData = canvas.toDataURL("image/png");
+             const pdf = new jsPDF("p", "mm", "a4", true);
+             const pdfWidth = pdf.internal.pageSize.getWidth();
+             const pdfHeight = pdf.internal.pageSize.getHeight();
+             const imageWidth = canvas.width;
+             const imageHeight = canvas.height;
+             const ratio = Math.min(pdfWidth / imageWidth, pdfHeight / imageHeight);
+             const imgX = (pdfWidth - imageWidth * ratio) / 2;
+             const imgY = 30;
+             pdf.addImage(
+               imgData,
+               "PNG",
+               imgX,
+               imgY,
+               imageWidth * ratio,
+               imageHeight * ratio
+             );
+             pdf.save(`shopitoAppInvoice.pdf`);
+           });
+    
+    
+    
+    
+    
+    }
   
   
    return (
     <section style={{height: "100vh"}}>
-       <div className='container'>
+       <div className='container' ref={pdfRef}>
           <h2>Order Details</h2>
           <div>
             <Link to="/order-history">&larr; Back To Orders</Link>
