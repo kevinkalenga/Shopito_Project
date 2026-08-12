0ceee7f4770d752237039a87fcdbbe2bfc359776
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-12 15:52:22 +0200

    Create Send Email Function

--- backend/utils/sendEmail.js
+++ backend/utils/sendEmail.js
@@ -1,6 +1,17 @@
 const nodemailer = require("nodemailer");
+const MailGen = require("mailgen")
 
-const sendEmail = async (to, subject, text) => {
+const mailGenerator = new MailGen({
+  theme: "salted",
+  product: {
+    name: "Shopito Website",
+    link: "http://localhost:3000",
+  },
+});
+
+
+
+const sendEmail = async (to, subject, text, reply_to, cc) => {
   try {
     const transporter = nodemailer.createTransport({
       service: "gmail",
@@ -10,13 +21,37 @@ const sendEmail = async (to, subject, text) => {
       },
     });
 
+    
+      const email = {
+        body: {
+          intro: text,
+          outro: "Merci d'avoir choisi Shopito !",
+        },
+      };
+
+      const html = mailGenerator.generate(email);
+    
+    
+    
+    
     const mailOptions = {
       from: `"Shopito" <${process.env.EMAIL_USER}>`,
       to,
       subject,
       text,
+      html
     };
 
+    if (reply_to) {
+      mailOptions.replyTo = reply_to;
+    }
+
+    if (cc) {
+      mailOptions.cc = cc;
+    }
+    
+    
+    
     const info = await transporter.sendMail(mailOptions);
 
     console.log("EMAIL SENT SUCCESS:", info.response);
