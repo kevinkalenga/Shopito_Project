f44c10dc7dc0eefc6939b450f0b4e029fdc54a68
kevin kalenga <kevinkalenga10@gmail.com>
2026-08-21 15:03:36 +0200

    Deposit Modal - JSX

--- frontend/src/pages/wallet/DepositModal.js
+++ frontend/src/pages/wallet/DepositModal.js
@@ -1,11 +1,81 @@
 import React from 'react'
 import "./DepositModal.scss"
+import { useRef, useEffect } from 'react';
+import { AiOutlineCloseCircle, AiOutlineInfoCircle } from "react-icons/ai";
 
 const DepositModal = ({depositData, closeModal, handleDepositChange, depositMoney}) => {
+  
+  const inputRef = useRef(null) 
+
+    useEffect(() => {
+      inputRef.current?.focus()
+    }, [])
+  
+  
+  
+  
+  
+  
   return (
-    <div>
-      <h1>Deposit Modal</h1>
-    </div>
+    <section className='--100vh modal-section'>
+      <div className='--flex-center modal'>
+         <div className='--bg-light --p --card modal-content'>
+            <AiOutlineCloseCircle color='red' size={16}
+             className='close-icon cm' onClick={(e) => closeModal(e)}/>
+             <div className='--flex-start modal-head --my'>
+               <AiOutlineInfoCircle color='orangered' size={18} />
+               <h3 className='--text-p --ml'>Send Money To Someone</h3>
+             </div>
+             <div className='modal-body'>
+                 <form onSubmit={depositMoney}>
+                    <p className='req'>
+                      <label>Amount</label>
+                      <input ref={inputRef} type='number' placeholder='Amount' name='amount'
+                       value={depositData.amount}
+                       onChange={handleDepositChange} 
+                       required/>
+                    </p>
+
+                    <p>
+                       <label htmlFor='stripe' className='radio-label'>
+                       <input className='radio-input' type='radio'
+                         name='paymentMethod'
+                         id='stripe'
+                         value={"stripe"}
+                         onChange={handleDepositChange} 
+                         required/>
+                         <span className='custom-radio' />
+                         Stripe
+                       </label>
+                    </p>
+                    <br />
+                    <p>
+                       <label htmlFor='flutterwave' className='radio-label'>
+                       <input className='radio-input' type='radio'
+                         name='paymentMethod'
+                         id='flutterwave'
+                         value={"flutterwave"}
+                         onChange={handleDepositChange} 
+                         required/>
+                         <span className='custom-radio' />
+                         Flutterwave
+                       </label>
+                    </p>
+                    <br />
+                    <span className='--flex-end'>
+                        <button className='--btn --btn-lg cm' onClick={(e) => closeModal(e)}>Cancel</button>
+                        <button type='submit' className='--btn --btn-primary cm'>
+                          Proceed
+                        </button>
+                    </span> 
+                   
+                    
+                 </form>
+             </div>
+         </div>
+      </div>
+    </section>
+
   )
 }
 
--- frontend/src/pages/wallet/TransferModal.js
+++ frontend/src/pages/wallet/TransferModal.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import "./TransferModal.scss"
 import { AiOutlineCloseCircle, AiOutlineInfoCircle } from "react-icons/ai";
-import { useRef } from 'react';
-import { useEffect } from 'react';
+import { useRef, useEffect } from 'react';
+
 import {useSelector} from "react-redux"
 import { selectReceiverName } from '../../redux/features/transaction/transactionSlice';
 
